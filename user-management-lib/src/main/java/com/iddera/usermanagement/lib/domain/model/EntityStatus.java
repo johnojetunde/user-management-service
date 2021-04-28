01626b4115bb7dc78ed420d5e8501f1b10908e6c
johnojetunde <johnojetunde63@gmail.com>
2021-04-28 03:22:44 +0300

    bumped version

--- user-management-lib/src/main/java/com/iddera/usermanagement/lib/domain/model/EntityStatus.java
+++ user-management-lib/src/main/java/com/iddera/usermanagement/lib/domain/model/EntityStatus.java
@@ -1,5 +1,22 @@
 package com.iddera.usermanagement.lib.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.stream.Stream;
+
+import static com.iddera.usermanagement.lib.domain.utils.FunctionUtil.isNullOrEmpty;
+
 public enum EntityStatus {
-    ACTIVE, INACTIVE
+    ACTIVE, INACTIVE, UNKNOWN;
+
+    @JsonCreator
+    public static EntityStatus create(String value) {
+        if (isNullOrEmpty(value)) {
+            return null;
+        }
+
+        return Stream.of(EntityStatus.values())
+                .filter(e -> e.toString().equalsIgnoreCase(value)).findFirst()
+                .orElse(UNKNOWN);
+    }
 }
---
+++ user-management-lib/src/main/java/com/iddera/usermanagement/lib/domain/utils/FunctionUtil.java
@@ -0,0 +1,32 @@
+package com.iddera.usermanagement.lib.domain.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FunctionUtil {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private FunctionUtil() {
+    }
+
+    static {
+        MAPPER.registerModule(new JavaTimeModule());
+
+    }
+
+    public static <T> Stream<T> emptyIfNullStream(Collection<T> list) {
+        return (list == null) ? Stream.empty() : list.stream();
+    }
+
+    public static <T> Collection<T> emptyIfNull(Collection<T> list) {
+        return (list == null) ? List.of() : list;
+    }
+
+    public static boolean isNullOrEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+}
