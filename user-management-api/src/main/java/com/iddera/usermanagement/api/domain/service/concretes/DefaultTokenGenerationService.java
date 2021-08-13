8e5f8abff7ca9281b493e9774e732c933a8eb562
sanmi <sanmiakinrele@gmail.com>
2021-08-13 14:15:23 +0100

    Changed token generation strategy.

--- user-management-api/src/main/java/com/iddera/usermanagement/api/domain/service/concretes/DefaultTokenGenerationService.java
+++ user-management-api/src/main/java/com/iddera/usermanagement/api/domain/service/concretes/DefaultTokenGenerationService.java
@@ -2,15 +2,15 @@ package com.iddera.usermanagement.api.domain.service.concretes;
 
 import com.iddera.usermanagement.api.domain.service.abstracts.TokenGenerationService;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
+import java.util.Random;
 
 @Service
-public class UuidTokenGenerationService implements TokenGenerationService {
+public class DefaultTokenGenerationService implements TokenGenerationService {
+    private static Integer TOKEN_BOUND = 999999;
+
     @Override
     public String generateToken() {
-        UUID uuid = UUID.randomUUID();
-
-        return uuid.toString();
+        Random rnd = new Random();
+        return String.format("%06d",rnd.nextInt(TOKEN_BOUND));
     }
 }
